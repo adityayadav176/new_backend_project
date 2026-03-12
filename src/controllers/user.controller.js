b0e6bd0b25d9ee3855b69d3dca006bf7904fe223
adityayadav176 <ay6598aditya@gmail.com>
2026-03-12 23:46:13 +0530

    changeCurrent Password endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -205,4 +205,25 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }
+const changeCurrentPassword = asyncHandler(async (req, res)=>{
+    const {oldPassword, newPassword, confPassword} = req.body  
+
+    if(!(newPassword === confPassword)){
+        throw new ApiError(400, " Required !Password must be same")
+    }
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "invalid Old Password while updating password")
+    }
+
+    user.password = newPassword
+    await user.save({ValidationBeforeSave: false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword }
