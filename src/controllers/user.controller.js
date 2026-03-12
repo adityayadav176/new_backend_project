19be5381086057c339f31c097b0a5d0917f7ee48
adityayadav176 <ay6598aditya@gmail.com>
2026-03-13 00:12:52 +0530

    updateCoverImage endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -268,6 +268,33 @@ const updateUserAvatar = asyncHandler(async(req, res) => {
     if(!user){
         throw new ApiError(400, {}, "something went wrong while updating avatar")
     }
+
+     return res
+    .status(200)
+    .json(new ApiResponse(200, user, "avatar updated successfully"))
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar }
+const updateUserCoverImage = asyncHandler(async(req, res) => {
+    const coverImageLocalPath = req.file?.path
+
+    if(!coverImageLocalPath){
+        throw new ApiError(400, "coverImage file is missing")
+    }
+
+    const coverImage = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(req.user?._id, {$set: {coverImage: coverImage.url}}, {new: true}).select("-password")
+
+    if(!user){
+        throw new ApiError(400, {}, "something went wrong while updating coverImage")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "coverImage updated successfully"))
+})
+export { registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage }
